96b611bd716101642c0779a5c9502c6da4651cb3
Mihary Joel <miharyjoel@gmail.com>
2023-04-17 16:58:33 +0300

    feat: add AbstractClass

--- src/main/java/com/mozartcoding/mozartcoding/abstractClass/AbstractClass1.java
+++ src/main/java/com/mozartcoding/mozartcoding/abstractClass/AbstractClass1.java
@@ -1,7 +1,13 @@
 package com.mozartcoding.mozartcoding.abstractClass;
 
+import com.mozartcoding.mozartcoding.concreteDependance.ConcreteDependance1;
+import com.mozartcoding.mozartcoding.concreteDependance.ConcreteDependance2;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class AbstractClass1 {
+    private ConcreteDependance1 concreteDependance1;
+    private ConcreteDependance2 concreteDependance2;
 }
